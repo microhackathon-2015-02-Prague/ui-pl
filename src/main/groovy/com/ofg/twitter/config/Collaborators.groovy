a9a616f0c5e7769230e5c43257d1b719d09c80ba
Jiri Novak <jiri@novakcz.net>
2015-02-23 16:39:59 +0100

    Dependencies and collaborators changes

--- src/main/groovy/com/ofg/twitter/config/Collaborators.groovy
+++ src/main/groovy/com/ofg/twitter/config/Collaborators.groovy
@@ -7,4 +7,12 @@ class Collaborators {
 
     public static final String COLLERATOR_DEPENDENCY_NAME = 'collerator'
 
+    public static final String DECISION_MAKER_DEPENDENCY_NAME = 'decisionMaker'
+
+    public static final String OFFER_GENERATOR_DEPENDENCY_NAME = 'offerGenerator'
+
+    public static final String CLIENT_SERVICE_DEPENDENCY_NAME = 'clientService'
+
+    public static final String LOAN_APP_SERVICE_DEPENDENCY_NAME = 'loanAppService'
+
 }
